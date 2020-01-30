a70ae5e0965b45ff614c1c873f491a1f0b120de5
Pablo <pablo.andrade@tty2000.com.br>
2020-01-30 10:18:49 -0300

    carregando imagem dos produtos

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -11,4 +11,9 @@ export class ProdutoService {
     findByProduto(categoria_id : string) {
         return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
     }
+
+    getSmallImageFromBucket(id: string) {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`;
+        return this.http.get(url, {responseType : 'blob'})
+    }
 }
