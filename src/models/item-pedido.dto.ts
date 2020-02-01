bd54f02ced51fe2cad5a05cf12cae62ef5fe5225
Pablo <pabllo007@gmail.com>
2020-02-01 18:19:46 -0300

    armazenamento dos dados do pedido

---
+++ src/models/item-pedido.dto.ts
@@ -0,0 +1,6 @@
+import { RefDTO } from "./Ref.dto";
+
+export interface ItemPedidoDTO {
+    quantidade : number;
+    produto: RefDTO;
+}
--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -3,6 +3,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { StorageService } from '../../services/storage.service';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoDTO } from './../../models/pedido.dto';
+import { ItemPedidoDTO } from './../../models/item-pedido.dto';
+import { CartService } from './../../services/domain/cart.service';
 
 @IonicPage()
 @Component({
@@ -13,11 +16,14 @@ export class PickAddressPage {
 
   items: EnderecoDTO[];
 
+  pedido: PedidoDTO;
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public cartService: CartService) {
   }
 
   ionViewDidLoad() {
@@ -26,6 +32,15 @@ export class PickAddressPage {
       this.clienteService.findByEmail(localUser.email)
       .subscribe(response => {
         this.items = response['enderecos'];
+        
+        let cart = this.cartService.getCart();
+
+        this.pedido = {
+          cliente: {id: response['id']},
+          enderecoDeEntrega: null,
+          pagamento: null,
+          itens: cart.items.map(x => {return {quantidade: x.quantidade, produto: {id: x.produto.id}}})
+        }
       },
       error => {
         if(error.status == 403) {
@@ -35,6 +50,11 @@ export class PickAddressPage {
     } else {
       this.navCtrl.setRoot('HomePage');
     }
+  }
+
+  nextPage(item: EnderecoDTO) {
+    this.pedido.enderecoDeEntrega = {id: item.id};
+    console.log(this.pedido);
   }
 
 }
