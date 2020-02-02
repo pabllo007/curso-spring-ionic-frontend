4da69b06c76e9da46f7402780b394cb7605866bb
Pablo <pabllo007@gmail.com>
2020-02-02 16:03:49 -0300

    Obtendo imagem da galeria

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseUrl : 'http://localhost:8080',
+    //baseUrl : 'http://localhost:8080',
+    baseUrl : 'https://curso-spring-ionic-pabllo007.herokuapp.com',
     bucketBaseUrl : "https://curso-spring-ionic-pablo.s3-sa-east-1.amazonaws.com"
 }
